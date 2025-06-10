49bd7fcb0405f51b3251f796803e75938d4ec3a7
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2025-06-10 17:12:45 -0300

    dados de bruno e isabela funcionais no backend

--- app/(tabs)/about/bruno.tsx
+++ app/(tabs)/about/bruno.tsx
@@ -1,9 +1,34 @@
+import AboutComponent from "@/components/AboutComponent";
+import PhotoCircle from "@/components/PhotoCircle";
+import { globalStyles } from "@/styles/globalStyle";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Bruno() {
     return (
-        <View>
-            <Text>Edit app/about/bruno.tsx to edit this screen.</Text>
-        </View>
+        <ScrollView contentContainerStyle={globalStyles.aboutContainer}>
+            <PhotoCircle imageSource={require('@/assets/images/devs/bruno.jpeg')} size={200}></PhotoCircle>
+            <AboutComponent>
+                <Text style={globalStyles.aboutText}>Bruno Guilherme é estudante do curso de Sistemas para Internet da Universidade Católica de Pernambuco (UNICAP).
+                    
+                </Text>
+            </AboutComponent>
+            <Text style={{ marginTop: 20, marginBottom: 20 }}>Conecte-se com Emmanuel Meireles!</Text>
+            <View style={globalStyles.connect}>
+                <Link href='https://www.linkedin.com/in/bruno-guilherme-da-silva/' target="_blank">
+                    <AntDesign name="linkedin-square" size={24} color="#083474" />
+                </Link>
+                <Link href="mailto:brunoguilherme@gmail.com"  target="_blank">
+                    <MaterialCommunityIcons name="email" size={24} color="#083474"/>
+                </Link>
+                <Link href="https://github.com/Bruno-Guilherme" target="_blank">
+                    <FontAwesome6 name="square-github" size={24} color="#083474"  />
+                </Link>
+            </View>
+        </ScrollView>
     );
 }
--- app/(tabs)/about/isabela.tsx
+++ app/(tabs)/about/isabela.tsx
@@ -1,10 +1,33 @@
+import AboutComponent from "@/components/AboutComponent";
+import PhotoCircle from "@/components/PhotoCircle";
+import { globalStyles } from "@/styles/globalStyle";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Isabela() {
     return (
-        
-        <View>
-            <Text>Edit app/about/isabela.tsx to edit this screen.</Text>
-        </View>
+        <ScrollView contentContainerStyle={globalStyles.aboutContainer}>
+            <PhotoCircle imageSource={require('@/assets/images/devs/emmanuel.jpg')} size={200}></PhotoCircle>
+            <AboutComponent>
+                <Text style={globalStyles.aboutText}>Isabela é estudante do curso de Sistemas para Internet da Universidade Católica de Pernambuco (UNICAP).
+                </Text>
+            </AboutComponent>
+            <Text style={{ marginTop: 20, marginBottom: 20 }}>Conecte-se com Emmanuel Meireles!</Text>
+            <View style={globalStyles.connect}>
+                <Link href='http://www.linkedin.com/in/isabela-araujo-s' target="_blank">
+                    <AntDesign name="linkedin-square" size={24} color="#083474" />
+                </Link>
+                <Link href="mailto:isabelacrvs@gmail.com"  target="_blank">
+                    <MaterialCommunityIcons name="email" size={24} color="#083474"/>
+                </Link>
+                <Link href="https://github.com/isacarvalho20" target="_blank">
+                    <FontAwesome6 name="square-github" size={24} color="#083474"  />
+                </Link>
+            </View>
+        </ScrollView>
     );
 }
